c615f8678d04a141a7a0fd2e395d6adba93854aa
Максим <homa19222@gmail.com>
2024-10-17 12:45:53 +0300

    commit
    my first servlet

---
+++ src/main/java/servlets/FirstServlet.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+    @Override protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        try (PrintWriter printWriter = resp.getWriter()){
+            printWriter.write("hello homasapienss");
+        }
+    }
+
+}
