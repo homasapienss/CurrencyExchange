e90f948b9851d10f45c1f8b479f077caf1273df2
Максим <homa19222@gmail.com>
2024-10-17 16:21:28 +0300

    commit
    обьявление сервлета без аннотации

--- src/main/java/servlets/FirstServlet.java
+++ src/main/java/servlets/FirstServlet.java
@@ -1,7 +1,6 @@
 package servlets;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/first")
+//@WebServlet("/first")
 public class FirstServlet extends HttpServlet {
     @Override protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
