35eb77b05e7e3904ead04f3c2b394cd04abadb80
Максим <homa19222@gmail.com>
2024-10-17 16:06:05 +0300

    commit
    my first servlet

--- src/main/java/servlets/FirstServlet.java
+++ src/main/java/servlets/FirstServlet.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 
 @WebServlet("/first")
